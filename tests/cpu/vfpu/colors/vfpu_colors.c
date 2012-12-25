a2946c46eb733b7480e1f1f0482f73104c3e3f6c
soywiz <soywiz@gmail.com>
2012-12-25 14:13:02 +0100

    - Small fix in vfpu_colors

--- tests/cpu/vfpu/colors/vfpu_colors.c
+++ tests/cpu/vfpu/colors/vfpu_colors.c
@@ -29,22 +29,22 @@ void resetAllMatrices() {
 	);
 }
 
-void vfpu_rgba8888to4444(int* in, short *out);
-void vfpu_rgba8888to5551(int* in, short *out);
-void vfpu_rgba8888to5650(int* in, short *out);
+void vfpu_rgba8888to4444(unsigned int* in, unsigned short *out);
+void vfpu_rgba8888to5551(unsigned int* in, unsigned short *out);
+void vfpu_rgba8888to5650(unsigned int* in, unsigned short *out);
 
 int main(int argc, char *argv[]) {
 	unsigned int in[4];
-	short out[4];
+	unsigned short out[4];
 	printf("Started\n");
 
 	resetAllMatrices();
 	
 	in[0] = 0xFFFF00FF; in[1] = 0x801100FF; in[2] = 0x7F5500FF; in[3] = 0x00aa00FF;
 
-	memset(out, 0, sizeof(out)); vfpu_rgba8888to4444(in, out); printf("vfpu_rgba8888to4444: %08X, %08X, %08X, %08X\n", out[0], out[1], out[2], out[3]);
-	memset(out, 0, sizeof(out)); vfpu_rgba8888to5551(in, out); printf("vfpu_rgba8888to5551: %08X, %08X, %08X, %08X\n", out[0], out[1], out[2], out[3]);
-	memset(out, 0, sizeof(out)); vfpu_rgba8888to5650(in, out); printf("vfpu_rgba8888to5650: %08X, %08X, %08X, %08X\n", out[0], out[1], out[2], out[3]);
+	memset(out, 0, sizeof(out)); vfpu_rgba8888to4444(in, out); printf("vfpu_rgba8888to4444: %04X, %04X, %04X, %04X\n", out[0], out[1], out[2], out[3]);
+	memset(out, 0, sizeof(out)); vfpu_rgba8888to5551(in, out); printf("vfpu_rgba8888to5551: %04X, %04X, %04X, %04X\n", out[0], out[1], out[2], out[3]);
+	memset(out, 0, sizeof(out)); vfpu_rgba8888to5650(in, out); printf("vfpu_rgba8888to5650: %04X, %04X, %04X, %04X\n", out[0], out[1], out[2], out[3]);
 	
 	printf("Ended\n");
 
