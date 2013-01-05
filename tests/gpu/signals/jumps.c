00fcc5e3049f3980f4ea177dd2302856ba2429a9
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-01-05 10:02:57 -0800

    Add some tests for jump/call/ret signals.

--- tests/gpu/signals/jumps.c
+++ tests/gpu/signals/jumps.c
@@ -10,6 +10,19 @@
 extern int sceGeContinue();
 extern int sceGeBreak(int breakType);
 
+typedef struct
+{
+    unsigned int stack[8];
+} PspGeStack;
+
+typedef struct
+{
+    u32 size;
+    PspGeContext *ctx;
+    u32 numStacks;
+    PspGeStack *stacks;
+} PspGeListArgs2;
+
 static unsigned int __attribute__((aligned(16))) list[262144];
 
 char buffer[65535];
@@ -31,14 +44,48 @@ unsigned int __attribute__((aligned(16))) dlist1[] = {
 	0x00000000, // 0x05 NOP
 	0x00000000, // 0x06 NOP
 	0x00000000, // 0x07 NOP
-	0x00000000, // 0x08 NOP
-	0x00000000, // 0x09 NOP
+	0x0E017777, // 0x08 SIGNAL + WAIT
+	0x0C000000, // 0x09 END
 	0x0F000000, // 0x0A FINISH
 	0x0C000000, // 0x0B END
-	0x00000000, // 0x0C NOP
-	0x00000000, // 0x0D NOP
+	0x0E018888, // 0x0C SIGNAL + WAIT
+	0x0C000000, // 0x0D END
 	0x0F000000, // 0x0E FINISH
 	0x0C000000, // 0x0F END
+
+	0x00000000, // 0x10 NOP
+	0x0E019999, // 0x11 SIGNAL + WAIT
+	0x0C000000, // 0x12 END
+	0x00000000, // 0x13 NOP
+	0x00000000, // 0x14 NOP
+	0x00000000, // 0x15 NOP
+	0x00000000, // 0x16 NOP
+	0x00000000, // 0x17 NOP
+	0x00000000, // 0x18 NOP
+	0x00000000, // 0x19 NOP
+	0x00000000, // 0x1A NOP
+	0x00000000, // 0x1B NOP
+	0x00000000, // 0x1C NOP
+	0x00000000, // 0x1D NOP
+	0x0F000000, // 0x1E FINISH
+	0x0C000000, // 0x1F END
+
+	0x00000000, // 0x20 NOP
+	0x0E01AAAA, // 0x21 SIGNAL + WAIT
+	0x0C000000, // 0x22 END
+	0x00000000, // 0x23 NOP
+	0x0E120000, // 0x24 SIGNAL RET
+	0x0C000000, // 0x25 END
+	0x00000000, // 0x26 NOP
+	0x00000000, // 0x27 NOP
+	0x00000000, // 0x28 NOP
+	0x00000000, // 0x29 NOP
+	0x00000000, // 0x2A NOP
+	0x00000000, // 0x2B NOP
+	0x00000000, // 0x2C NOP
+	0x00000000, // 0x2D NOP
+	0x0F000000, // 0x2E FINISH
+	0x0C000000, // 0x2F END
 };
 
 int dlist1id, dlist2id;
@@ -170,11 +217,22 @@ enum {
 
 void testGeCallbacks(int method) {
 	int listsync, result;
+	PspGeListArgs2 args;
+	PspGeContext saved;
+	PspGeStack stacks[32];
+
+	sceGeSaveContext(&saved);
+
+	args.size = sizeof(args);
+	args.ctx = &saved;
+	args.numStacks = 1;
+	args.stacks = &stacks[0];
+
 	sceKernelDcacheWritebackAll();
 
 	bpos += sprintf(bpos, "  LIST #\tDRAWSTATE\tINFO\n");
 
-	dlist1id = sceGeListEnQueue(dlist1, 0, cbid1, 0);
+	dlist1id = sceGeListEnQueue(dlist1, 0, cbid1, (PspGeListArgs *) &args);
 	listInfoNosync(1, "Enqueued without stall...\n");
 	listsync = sceGeListSync(dlist1id, 1);
 	listInfo(1, "Sync %x %s\n", listsync, status_str(listsync));
@@ -183,7 +241,7 @@ void testGeCallbacks(int method) {
 	listsync = sceGeListSync(dlist1id, 1);
 	listInfo(1, "Sync %x %s after continue (%08x)\n", listsync, status_str(listsync), result);
 
-	dlist2id = sceGeListEnQueue(dlist1, method & TEST_STALL_LATE ? dlist1 + 0x0D : dlist1 + 0x05, cbid2, 0);
+	dlist2id = sceGeListEnQueue(dlist1, method & TEST_STALL_LATE ? dlist1 + 0x0D : dlist1 + 0x05, cbid2, (PspGeListArgs *) &args);
 	listInfoNosync(2, "Enqueued with %s...\n", method & TEST_STALL_LATE ? "late stall" : "stall");
 	listsync = sceGeListSync(dlist2id, 1);
 	listInfo(2, "Sync %x %s\n", listsync, status_str(listsync));
@@ -218,74 +276,81 @@ enum
 	GE_SIG_PAUSE = 0x03,
 	GE_SIG_SYNC = 0x08,
 
+	GE_SIG_JUMP = 0x10,
+	GE_SIG_CALL = 0x11,
+	GE_SIG_JUMP_REL = 0x13,
+	GE_SIG_CALL_REL = 0x14,
+	GE_SIG_JUMP_ORIGIN = 0x15,
+	GE_SIG_CALL_ORIGIN = 0x16,
+
 	GE_SIG_BP1 = 0xF0,
 	GE_SIG_BP2 = 0xFF,
 };
 
-#define MAKE_GE_END(type, value) (0x0C000000 | ((type & 0xFF) << 16) | (value & 0xFFFF))
-#define MAKE_GE_SIGNAL(type, value) (0x0E000000 | ((type & 0xFF) << 16) | (value & 0xFFFF))
-#define MAKE_GE_FINISH(type, value) (0x0F000000 | ((type & 0xFF) << 16) | (value & 0xFFFF))
+#define MAKE_GE_END(type, value) (0x0C000000 | ((type & 0xFF) << 16) | ((value) & 0xFFFF))
+#define MAKE_GE_SIGNAL(type, value) (0x0E000000 | ((type & 0xFF) << 16) | ((value) & 0xFFFF))
+#define MAKE_GE_FINISH(type, value) (0x0F000000 | ((type & 0xFF) << 16) | ((value) & 0xFFFF))
+#define MAKE_GE_BASE(address) (0x10000000 | (((address) & 0xFF000000) >> 8))
+#define MAKE_GE_ORIGIN(value) (0x14000000 | ((value) & 0xFFFFFF))
 
-void testSignalTypes() {
-	// All invalid callbacks should work this way? (0x04, 0xFE, etc.)
-	printf("\nUnknown (0x%02x):\n", 0);
-	dlist1[1] = MAKE_GE_SIGNAL(0, 0x1234);
-	dlist1[2] = MAKE_GE_END(0, 0);
-	testGeCallbacks(TEST_USE_BREAK | TEST_STALL_LATE);
+inline void dlist1SignalOffset(int pos, int type, int endtype, unsigned int address) {
+	dlist1[pos + 0x00] = MAKE_GE_SIGNAL(type, address >> 16);
+	dlist1[pos + 0x01] = MAKE_GE_END(endtype, address);
+}
 
-	// SUSPEND list while in handler
-	printf("\nSignal handler + wait (0x%02x):\n", GE_SIG_SUSPEND);
-	dlist1[0x01] = MAKE_GE_SIGNAL(GE_SIG_SUSPEND, 0x1234);
-	dlist1[0x02] = MAKE_GE_END(0, 0);
+inline void dlist1SignalRelative(int pos, int type, int endtype, unsigned int address) {
+	dlist1SignalOffset(pos, type, endtype, address * sizeof(unsigned int));
+}
+
+inline void dlist1SignalAddress(int pos, int type, int endtype, unsigned int *address) {
+	dlist1SignalOffset(pos, type, endtype, (unsigned int) address);
+}
+
+void testSignalCallsAndJumps() {
+	printf("\nJump absolute (0x%02x):\n", GE_SIG_JUMP);
+	dlist1SignalAddress(0x01, GE_SIG_JUMP, 0, dlist1 + 0x10);
 	testGeCallbacks(TEST_USE_DRAWSYNC);
 
-	// CONTINUE list keeps getting processed
-	printf("\nSignal handler only (0x%02x):\n", GE_SIG_CONTINUE);
-	dlist1[0x01] = MAKE_GE_SIGNAL(GE_SIG_CONTINUE, 0x1234);
-	dlist1[0x02] = MAKE_GE_END(0, 0);
+	printf("\nJump relative (0x%02x):\n", GE_SIG_JUMP_REL);
+	dlist1SignalRelative(0x01, GE_SIG_JUMP_REL, 0, 0x10 - 0x01);
 	testGeCallbacks(TEST_USE_DRAWSYNC);
 
-	// PAUSE list and call handler at FINISH CMD, but can resume
-	printf("\nSignal handler + pause (0x03):\n");
-	dlist1[0x01] = MAKE_GE_SIGNAL(GE_SIG_PAUSE, 0x1234);
-	dlist1[0x02] = MAKE_GE_END(0, 0);
-	testGeCallbacks(TEST_USE_DRAWSYNC | TEST_STALL_LATE);
+	printf("\nJump relative to origin (0x%02x):\n", GE_SIG_JUMP_ORIGIN);
+	// Base seems not to matter, just proving that here.
+	dlist1[0x01] = MAKE_GE_BASE(0);
+	dlist1[0x02] = MAKE_GE_ORIGIN(0);
+	dlist1SignalRelative(0x03, GE_SIG_JUMP_ORIGIN, 0, 0x10 - 0x02);
+	testGeCallbacks(TEST_USE_DRAWSYNC);
+	dlist1[0x03] = 0;
+	dlist1[0x04] = 0;
 
-	// SYNC doesn't call handler but syncs and skips
-	printf("\nSync + continue (0x%02x):\n", GE_SIG_SYNC);
-	dlist1[0x01] = MAKE_GE_SIGNAL(GE_SIG_SYNC, 0x1234);
-	dlist1[0x02] = MAKE_GE_END(0, 0);
-	testGeCallbacks(TEST_USE_DRAWSYNC | TEST_STALL_LATE);
-
-	// Not included in this test - 0x10 - 0x16 (call/jump/ret/etc.)
-	// Not included in this test - 0x20 - 0x38 (tex/clut/etc.)
-
-	// BREAKPOINT may not do anything?
-	printf("\nBreakpoint #1 (0x%02x):\n", GE_SIG_BP1);
-	dlist1[0x01] = MAKE_GE_SIGNAL(GE_SIG_BP1, 0x1234);
-	dlist1[0x02] = MAKE_GE_END(0, 0);
+	printf("\nCall absolute (0x%02x):\n", GE_SIG_CALL);
+	dlist1SignalAddress(0x01, GE_SIG_CALL, 0, dlist1 + 0x20);
 	testGeCallbacks(TEST_USE_DRAWSYNC);
 
-	printf("\nUnknown (0x%02x):\n", 0xEE);
-	dlist1[1] = MAKE_GE_SIGNAL(0xEE, 0x1234);
-	dlist1[2] = MAKE_GE_END(0, 0);
-	testGeCallbacks(TEST_USE_BREAK | TEST_STALL_LATE);
-
-	printf("\nBreakpoint #2 (0x%02x):\n", GE_SIG_BP2);
-	dlist1[0x01] = MAKE_GE_SIGNAL(GE_SIG_BP2, 0x1234);
-	dlist1[0x02] = MAKE_GE_END(0, 0);
+	printf("\nCall relaive (0x%02x):\n", GE_SIG_CALL_REL);
+	dlist1SignalRelative(0x01, GE_SIG_CALL_REL, 0, 0x20 - 0x01);
 	testGeCallbacks(TEST_USE_DRAWSYNC);
+
+	printf("\nCall relative to origin (0x%02x):\n", GE_SIG_CALL_ORIGIN);
+	// Base seems not to matter, just proving that here.
+	dlist1[0x01] = MAKE_GE_BASE(0);
+	dlist1[0x02] = MAKE_GE_ORIGIN(0);
+	dlist1SignalRelative(0x03, GE_SIG_CALL_ORIGIN, 0, 0x20 - 0x02);
+	testGeCallbacks(TEST_USE_DRAWSYNC);
+	dlist1[0x03] = 0;
+	dlist1[0x04] = 0;
 }
 
 int main(int argc, char *argv[]) {
 	printf("Using default SDK version:\n");
 	init();
-	testSignalTypes();
+	testSignalCallsAndJumps();
 
 	printf("\n\nUsing 6.60 SDK version:\n");
 	sceKernelSetCompiledSdkVersion(0x6060010);
 	init();
-	testSignalTypes();
+	testSignalCallsAndJumps();
 
 	return 0;
 }
