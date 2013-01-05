86c8dccb99b7cbb6bd324d7d34c0916eb29759bd
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-01-05 02:05:28 -0800

    Create a simplified Makefile include.

---
+++ common/common.mk
@@ -0,0 +1,48 @@
+# Include this to simplify your test include file.
+#
+# Usage:
+#
+# TARGETS=mytest
+# include ../../path/to/common/common.mk
+
+COMMON_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+BUILD_PRX = 1
+USE_PSPSDK_LIBC = 1
+PSP_FW_VERSION = 500
+
+INCDIR := $(INCDIR) . $(COMMON_DIR)
+LIBDIR := $(LIBDIR) . $(COMMON_DIR)
+
+ifndef CFLAGS
+CFLAGS = -g -G0 -Wall -O0 -fno-strict-aliasing
+endif
+ifndef CXXFLAGS
+CXXFLAGS = $(CFLAGS) -fno-exceptions -fno-rtti 
+endif
+ifndef ASFLAGS
+ASFLAGS = $(CFLAGS)
+endif
+ifndef LDFLAGS
+LDFLAGS = -G0
+endif
+
+ifndef LIBS
+LIBS = -lpspgu -lpsprtc -lpspctrl -lpspmath -lcommon -lc -lm
+endif
+
+TARGET = $(firstword $(TARGETS))
+OBJS = $(firstword $(TARGETS)).o
+
+PSPSDK = $(shell psp-config --pspsdk-path)
+include $(PSPSDK)/lib/build.mak
+
+%.elf: %.o $(EXTRA_OBJS) $(EXPORT_OBJ)
+	$(LINK.c) $^ $(LIBS) -o $@
+	$(FIXUP) $@
+
+%.prx: %.elf
+	psp-prxgen $< $@
+
+all: $(TARGETS:=.prx)
+clean: EXTRA_TARGETS:=$(EXTRA_TARGETS) $(TARGETS:=.prx)
