90be520fceff14f58fde21cf9650103865560549
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-02-23 19:28:21 -0800

    Update and fix some misc tests.

--- tests/sysmem/sysmem.c
+++ tests/sysmem/sysmem.c
@@ -4,26 +4,52 @@
 #include <pspsysmem.h>
 #include <pspthreadman.h>
 
+// printf() seems to reschedule, so can't use it.
+static char schedulingLog[65536];
+static volatile int schedulingLogPos = 0;
+
+inline void schedf(const char *format, ...) {
+	va_list args;
+	va_start(args, format);
+	schedulingLogPos += vsprintf(schedulingLog + schedulingLogPos, format, args);
+	// This is easier to debug in the emulator, but printf() reschedules on the real PSP.
+	//vprintf(format, args);
+	va_end(args);
+}
+
+inline void flushschedf() {
+	printf("%s", schedulingLog);
+	schedulingLogPos = 0;
+	schedulingLog[0] = '\0';
+}
+
 int testThreadStackFillsWith_0xFF_AndIsAlignedTo_0x10__thread(SceSize arglen, void *argp) {
+	sceKernelSleepThread();
+
+	return 0;
+}
+
+void testThreadStackFillsWith_0xFF_AndIsAlignedTo_0x10() {
 	int n, FFLen = 0;
-	SceUID thid = sceKernelGetThreadId();
 	SceKernelThreadInfo info;
+
+	printf("testThreadStackFillsWith_0xFF_AndIsAlignedTo_0x10:\n");
+	SceUID thid = sceKernelCreateThread("my_thread", &testThreadStackFillsWith_0xFF_AndIsAlignedTo_0x10__thread, 0x18, 0x201, 0, NULL);
+	sceKernelStartThread(thid, 0, NULL);
+
+	info.size = sizeof(info);
 	sceKernelReferThreadStatus(thid, &info);
-	//printf("StackStart: %08X\n", ((unsigned int)info.stack)/* - 0x08900000*/);
+	schedf("StackStart: %08X\n", (unsigned int)((char *)info.stack - (char *)&info));
 	printf("StackSize   : %08X\n", info.stackSize);
 	for (n = 0; n < info.stackSize; n++) {
 		if (((unsigned char *)info.stack)[-n] == 0xFF) FFLen++;
 	}
 	printf("FFLen       : %08X\n", FFLen);
-	printf("Aligned_0x10: %s\n"  , (((unsigned int)info.stack) & 0xF) ? "yes" : "no");
-	return 0;
-}
+	printf("Aligned_0x10: %s\n"  , (((unsigned int)info.stack) & 0xF) == 0 ? "yes" : "no");
 
-void testThreadStackFillsWith_0xFF_AndIsAlignedTo_0x10() {
-	printf("testThreadStackFillsWith_0xFF_AndIsAlignedTo_0x10:\n");
-	SceUID thid = sceKernelCreateThread("my_thread", testThreadStackFillsWith_0xFF_AndIsAlignedTo_0x10__thread, 0x18, 0x201, 0, NULL);
-	sceKernelStartThread(thid, 0, NULL);
+	sceKernelWakeupThread(thid);
 	sceKernelWaitThreadEnd(thid, NULL);
+	flushschedf();
 }
 
 int main(int argc, char **argv) {
