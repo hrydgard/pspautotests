6b29fc814f9052c987b73f8421c1fc9b522893a1
Unknown W. Brackets <checkins@unknownbrackets.org>
2014-02-02 10:07:15 -0800

    Add more tests for vpl thread priorities.

--- tests/threads/vpl/priority.c
+++ tests/threads/vpl/priority.c
@@ -1,31 +1,63 @@
 #include "shared.h"
 
+typedef enum {
+	TEST_TYPE_WHOLE,
+	TEST_TYPE_HALF,
+	TEST_TYPE_TRY,
+} TestType;
+
+TestType g_testType;
 SceUID vpl;
 
 int threadFunction(SceSize argc, void *argv) {
 	int n = *(int *) argv;
 	void *data;
 
-	schedf("  [%d] Waiting...\n", n);
-	int result = sceKernelAllocateVpl(vpl, 0x900 - n * 0x10, &data, NULL);
-	schedf("  [%d] Allocated.\n", n);
+	int sz = 0x900 - n * 0x100;
+	schedf("      [%d] Waiting (%x)...\n", n, sz);
 
-	schedf("    ");
+	int tryResult = -1;
+	int result = -1;
+	if (g_testType == TEST_TYPE_TRY) {
+		tryResult = sceKernelTryAllocateVpl(vpl, sz, &data);
+		schedf("      [%d] Try Allocated (%x -> %08x).\n", n, sz, tryResult);
+	}
+	if (tryResult < 0) {
+		result = sceKernelAllocateVpl(vpl, sz, &data, NULL);
+	} else {
+		result = tryResult;
+	}
+
+	int intr = sceKernelCpuSuspendIntr();
+	schedf("      [%d] Allocated (%x -> %08x).\n", n, sz, result);
+
+	schedf("        ");
 	schedfVpl(vpl);
+	sceKernelCpuResumeIntr(intr);
 
+	schedf("      [%d] Freeing (%x)...\n", n, sz);
 	sceKernelFreeVpl(vpl, data);
 	return 0;
 }
 
-void testPriority(u32 attr) {
-	schedf("Attr 0x%x:\n", attr);
+void testPriority(const char *title, u32 attr, TestType testType) {
+	g_testType = testType;
+
+	checkpointNext(title);
 	vpl = sceKernelCreateVpl("vpl", PSP_MEMORY_PARTITION_USER, attr, 0x1000 + 0x20, NULL);
 
 	SceUID threads[7];
 	int numbers[7] = {1, 2, 3, 4, 5, 6, 7};
 
-	void *data;
-	sceKernelAllocateVpl(vpl, 0x900, &data, NULL);
+	void *data1;
+	void *data2;
+	checkpoint("  [ ] Blocked");
+	if (testType == TEST_TYPE_HALF) {
+		sceKernelAllocateVpl(vpl, 0x780, &data1, NULL);
+		sceKernelAllocateVpl(vpl, 0x780, &data2, NULL);
+	} else {
+		sceKernelAllocateVpl(vpl, 0x980, &data1, NULL);
+	}
 
 	int i;
 	for (i = 0; i < 7; i++) {
@@ -34,26 +66,43 @@ void testPriority(u32 attr) {
 	}
 
 	sceKernelDelayThread(1000);
-	schedf("  [0] Ready.\n");
-	sceKernelFreeVpl(vpl, data);
+	if (testType == TEST_TYPE_HALF) {
+		checkpoint("  [ ] Unblock half");
+		sceKernelFreeVpl(vpl, data2);
+		checkpoint("  [ ] Unblock whole");
+		sceKernelFreeVpl(vpl, data1);
+	} else {
+		checkpoint("  [ ] Unblock");
+		sceKernelFreeVpl(vpl, data1);
+	}
 
-	sceKernelDelayThread(20 * 1000);
+	sceKernelDelayThread(100 * 1000);
 
+	checkpoint("  [ ] Deleting");
 	sceKernelDeleteVpl(vpl);
 
 	for (i = 0; i < 7; i++) {
 		sceKernelTerminateDeleteThread(threads[i]);
 	}
 
-	flushschedf();
+	checkpoint("Done");
 }
 
 int main(int argc, char **argv) {
-	testPriority(PSP_VPL_ATTR_FIFO);
-	schedf("\n");
-	testPriority(PSP_VPL_ATTR_PRIORITY);
-	schedf("\n");
-	// Does nothing?
-	testPriority(PSP_VPL_ATTR_SMALLEST);
+	testPriority("FIFO/whole:", PSP_VPL_ATTR_FIFO, TEST_TYPE_WHOLE);
+	testPriority("PRIORITY/whole:", PSP_VPL_ATTR_PRIORITY, TEST_TYPE_WHOLE);
+	testPriority("SMALLEST/whole:", PSP_VPL_ATTR_SMALLEST, TEST_TYPE_WHOLE);
+	testPriority("PRIORITY|SMALLEST/whole:", PSP_VPL_ATTR_PRIORITY | PSP_VPL_ATTR_SMALLEST, TEST_TYPE_WHOLE);
+
+	testPriority("FIFO/half:", PSP_VPL_ATTR_FIFO, TEST_TYPE_HALF);
+	testPriority("PRIORITY/half:", PSP_VPL_ATTR_PRIORITY, TEST_TYPE_HALF);
+	testPriority("SMALLEST/half:", PSP_VPL_ATTR_SMALLEST, TEST_TYPE_HALF);
+	testPriority("PRIORITY|SMALLEST/half:", PSP_VPL_ATTR_PRIORITY | PSP_VPL_ATTR_SMALLEST, TEST_TYPE_HALF);
+
+	testPriority("FIFO/try:", PSP_VPL_ATTR_FIFO, TEST_TYPE_TRY);
+	testPriority("PRIORITY/try:", PSP_VPL_ATTR_PRIORITY, TEST_TYPE_TRY);
+	testPriority("SMALLEST/try:", PSP_VPL_ATTR_SMALLEST, TEST_TYPE_TRY);
+	testPriority("PRIORITY|SMALLEST/try:", PSP_VPL_ATTR_PRIORITY | PSP_VPL_ATTR_SMALLEST, TEST_TYPE_TRY);
+
 	return 0;
 }
